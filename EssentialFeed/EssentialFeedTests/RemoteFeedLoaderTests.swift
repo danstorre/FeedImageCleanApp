ca74cf96247469bd5a43011dcbc7a46d9a3e4b1b
Daniel Torres <danstorre@gmail.com>
2021-06-09 15:27:20 -0400

    add client to remote feed loader and check if the http client is calling the right url from the remote feed loader.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,19 +1,49 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
     
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.load(from: url)
+    }
 }
 
 class HTTPClient {
-    let requestedURL: URL? = nil
+    var requestedURL: URL? = nil
+    
+    func load(from url: URL? = URL(string: "http://")) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesntMakeAnyRequests() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_doesMakeTheRequestNeed() {
+        let requestURL = URL(string: "http://another.com")!
+        let (sut, client) = makeSUT(requestURL: requestURL)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(requestURL, client.requestedURL)
+    }
+    
+    func makeSUT(requestURL: URL = URL(string: "http://another.com")!) -> (RemoteFeedLoader, HTTPClient){
+        let client = HTTPClient()
+        let sut = RemoteFeedLoader(client: client, url: requestURL)
+        
+        return (sut, client)
     }
 }
