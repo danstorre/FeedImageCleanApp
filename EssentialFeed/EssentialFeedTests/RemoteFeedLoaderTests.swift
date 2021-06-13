5d43da89f1e588774318364ffc299c2b2b4445e6
Daniel Torres <danstorre@gmail.com>
2021-06-13 14:28:33 -0400

    used an array of values in the test spy to simplify the number of times load is being called.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,7 +6,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
 
-        XCTAssertNil(client.requestedURL)
+        XCTAssert(client.requestedURLs.isEmpty)
     }
 
     func test_load_requestsDataFromURL() {
@@ -15,7 +15,17 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         sut.load()
 
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs[0], url)
+    }
+    
+    func test_call_load_twice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
     // MARK: - Helpers
@@ -27,10 +37,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedURLs: [URL] = []
 
         func get(from url: URL) {
-            requestedURL = url
+            requestedURLs.append(url)
         }
     }
 
