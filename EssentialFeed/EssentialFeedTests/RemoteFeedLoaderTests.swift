8d0f1c5f38e50837e0d99030deb55723e4253e52
Daniel Torres <danstorre@gmail.com>
2021-06-22 11:26:27 -0300

    use explicit data for non 200 http response tests.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -43,7 +43,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199,201,300,400,500]
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: .failure( .invalidData)) {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             }
         }
     }
@@ -144,7 +145,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let httpResponse = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
