9444b45e0dd2df93eca7b6b74935d0bb0edfac68
Daniel Torres <danstorre@gmail.com>
2021-06-22 11:44:42 -0300

    move mapping logic to a new type `FeedItemsMapper`

--- EssentialFeed/EssentialFeed/Remote Feed Loader/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Remote Feed Loader/RemoteFeedLoader.swift
@@ -32,9 +32,10 @@ public final class RemoteFeedLoader {
         client.get(from: url) { response  in
             switch response {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map({ $0.item })))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -44,17 +45,27 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var item: FeedItem {
-        FeedItem(id: id, description: description, location: location, imageURL: image)
+    struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map({ $0.item })
     }
 }
