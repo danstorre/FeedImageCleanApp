1a24039b747a8f812c5fcfe258638a54d527eeca
Daniel Torres <danstorre@gmail.com>
2021-12-16 09:26:48 -0300

    pump up deployment version to 11.0.

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -31,7 +31,7 @@ public final class LocalFeedLoader {
     }
 }
 
-public extension Array where Element == FeedItem {
+private extension Array where Element == FeedItem {
     func toLocal() -> [LocalFeedItem] {
         self.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
     }
