b860fc2faadc91d7f4a56047e3c6e0d83a6201c2
Daniel Torres <danstorre@gmail.com>
2022-05-11 18:55:49 -0300

    Writes a Production report csv appending the total lines of swift code.

--- report.sh
+++ report.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-find -s EssentialFeed/EssentialFeed -iname "*.swift" -print0 -type f | xargs -0 wc -l | awk '{print $1}' | tail -n 1
+
+# Production report
+production_file_name="Production report.csv"
+
+echo "Indicators, Now, Desired" > $production_file_name
+
+# Add total lines of code from all swift files of the production side
+LOC=$(find -s EssentialFeed/EssentialFeed -iname "*.swift" -print0 -type f | xargs -0 wc -l | awk '{print $1}' | tail -n 1)
+echo "Total LOC, $LOC, N/A" >> $production_file_name
