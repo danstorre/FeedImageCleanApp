031853ae7ecf86030ec1e0b3269dc21a84ff127e
Daniel Torres <danstorre@gmail.com>
2022-05-11 21:59:41 -0300

    add fix comment count.

--- report.sh
+++ report.sh
@@ -19,3 +19,7 @@ echo "Swift file count, $(($LOC/$SFC)), <100" >> $production_file_name
 # Add TODO comment count
 TODOC=$(find -s EssentialFeed/EssentialFeed -iname "*.swift" -print0 -type f | xargs -0 grep TODO | wc -l)
 echo "TODO comment count, $TODOC, 0" >> $production_file_name
+
+# Add FIX comment count
+FIXC=$(find -s EssentialFeed/EssentialFeed -iname "*.swift" -print0 -type f | xargs -0 grep FIX | wc -l)
+echo "FIX comment count, $FIXC, 0" >> $production_file_name
