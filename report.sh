579f1aba90fe3818021649700c7473dc694ef2ed
Daniel Torres <danstorre@gmail.com>
2022-05-11 21:42:00 -0300

    appends Swift file count

--- report.sh
+++ report.sh
@@ -8,3 +8,7 @@ echo "Indicators, Now, Desired" > $production_file_name
 # Add total lines of code from all swift files of the production side
 LOC=$(find -s EssentialFeed/EssentialFeed -iname "*.swift" -print0 -type f | xargs -0 wc -l | awk '{print $1}' | tail -n 1)
 echo "Total LOC, $LOC, N/A" >> $production_file_name
+
+# Add Swift file count
+SFC=$(find -s EssentialFeed/EssentialFeed -iname "*.swift" -type f | wc -l)
+echo "Swift file count, $SFC, N/A" >> $production_file_name
