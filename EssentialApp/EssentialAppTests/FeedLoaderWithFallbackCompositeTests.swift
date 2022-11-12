f3dbe54dc933244c17d3f7e512d7b1ab085f837f
Daniel Torres <danstorre@gmail.com>
2022-11-11 23:28:02 -0400

    `FeedLoaderWithFallbackComposite.load` delivers primary feed on primary loader success.

---
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,56 @@
+import XCTest
+import EssentialFeed
+
+class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+
+    init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load(completion: completion)
+    }
+}
+
+final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
+    
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
+        let primaryFeed = uniqueFeed()
+        let fallbackFeed = uniqueFeed()
+        let primaryLoader = LoaderStub(result: .success(primaryFeed))
+        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        let exp = expectation(description: "Wait for load completioin")
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, primaryFeed)
+                
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func uniqueFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+    }
+    
+    private class LoaderStub: FeedLoader {
+        private let result: FeedLoader.Result
+        
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completion(result)
+        }
+    }
+}
