69ba47cee7622bf9fe0e773598a199aa3a251086
Daniel Torres <danstorre@gmail.com>
2022-11-16 21:16:23 -0400

    Configures feed navigation as window root view controller

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -16,7 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
-		
+        
+        configureWindow()
+    }
+    
+    func configureWindow() {
         let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         
         let remoteClient = makeRemoteClient()
@@ -27,17 +31,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
         
-        window?.rootViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: FeedLoaderWithFallbackComposite(
-                primary: FeedLoaderCacheDecorator(
-                    decoratee: remoteFeedLoader,
-                    cache: localFeedLoader),
-                fallback: localFeedLoader),
-            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-                primary: localImageLoader,
-                fallback: FeedImageDataLoaderCacheDecorator(
-                    decoratee: remoteImageLoader,
-                    cache: localImageLoader)))
+        window?.rootViewController = UINavigationController(
+            rootViewController: FeedUIComposer.feedComposedWith(
+                feedLoader: FeedLoaderWithFallbackComposite(
+                    primary: FeedLoaderCacheDecorator(
+                        decoratee: remoteFeedLoader,
+                        cache: localFeedLoader),
+                    fallback: localFeedLoader),
+                imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                    primary: localImageLoader,
+                    fallback: FeedImageDataLoaderCacheDecorator(
+                        decoratee: remoteImageLoader,
+                        cache: localImageLoader))))
 	}
     
     func makeRemoteClient() -> HTTPClient {           
---
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+import EssentialFeediOS
+@testable import EssentialApp
+
+final class SceneDelegateTests: XCTestCase {
+    
+    func test_sceneWillConnectToSession_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindow()
+        
+        sut.configureWindow()
+        
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+        
+        XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
+        XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+}
