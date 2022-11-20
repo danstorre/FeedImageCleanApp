cdee7de1f83860b1f7a0f2c944d14099eb411eea
Daniel Torres <danstorre@gmail.com>
2022-11-20 14:35:34 -0400

    test_configureWindow_setsWindowAsKeyAndVisible not passing. Expected window to be the key window.

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -33,8 +33,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
 
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-		guard let _ = (scene as? UIWindowScene) else { return }
+		guard let scene = (scene as? UIWindowScene) else { return }
         
+        window = UIWindow(windowScene: scene)
         configureWindow()
     }
     
@@ -58,6 +59,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: FeedImageDataLoaderCacheDecorator(
                         decoratee: remoteImageLoader,
                         cache: localImageLoader))))
+        
+        window?.makeKey()
 	}
     
     func sceneWillResignActive(_ scene: UIScene) {
--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -4,6 +4,16 @@ import EssentialFeediOS
 
 final class SceneDelegateTests: XCTestCase {
     
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindow()
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
+    }
+    
     func test_sceneWillConnectToSession_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
