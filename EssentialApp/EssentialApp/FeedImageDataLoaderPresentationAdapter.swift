086d1134427c8276eddfe86eb58a81ca3dabc597
Daniel Torres <danstorre@gmail.com>
2022-11-23 22:41:21 -0400

    Move dispatchOnMainQueue operator to the point of use (before subscription with `sink`)

--- EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
@@ -8,36 +8,38 @@ import EssentialFeed
 import EssentialFeediOS
 
 final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-	private let model: FeedImage
+    private let model: FeedImage
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
     private var cancellable: Cancellable?
-	
-	var presenter: FeedImagePresenter<View, Image>?
-	
+    
+    var presenter: FeedImagePresenter<View, Image>?
+    
     init(model: FeedImage, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
-		self.model = model
-		self.imageLoader = imageLoader
-	}
-	
-	func didRequestImage() {
-		presenter?.didStartLoadingImageData(for: model)
-		
-		let model = self.model
-        cancellable = imageLoader(model.url).sink(
-            receiveCompletion: { [weak self] completion in
-                switch completion {
-                case .finished: break
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+        
+        let model = self.model
+        cancellable = imageLoader(model.url)
+            .dispatchOnMainQueue()
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished: break
+                        
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoadingImageData(with: error, for: model)
+                    }
                     
-                case let .failure(error):
-                    self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-                }
-                
-            }, receiveValue: { [weak self] data in
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-            })
-	}
-	
-	func didCancelImageRequest() {
+                }, receiveValue: { [weak self] data in
+                    self?.presenter?.didFinishLoadingImageData(with: data, for: model)
+                })
+    }
+    
+    func didCancelImageRequest() {
         cancellable?.cancel()
-	}
+    }
 }
--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -7,31 +7,34 @@ import EssentialFeed
 import EssentialFeediOS
 
 public final class FeedUIComposer {
-	private init() {}
-	
-    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
-		let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: { feedLoader().dispatchOnMainQueue() })
-		
-		let feedController = makeFeedViewController(
-			delegate: presentationAdapter,
-			title: FeedPresenter.title)
-
-		presentationAdapter.presenter = FeedPresenter(
-			feedView: FeedViewAdapter(
-				controller: feedController,
-                imageLoader: { imageLoader($0).dispatchOnMainQueue() }),
-			loadingView: WeakRefVirtualProxy(feedController),
-			errorView: WeakRefVirtualProxy(feedController))
-		
-		return feedController
-	}
-
-	private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
-		let bundle = Bundle(for: FeedViewController.self)
-		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-		let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-		feedController.delegate = delegate
-		feedController.title = title
-		return feedController
-	}
+    private init() {}
+    
+    public static func feedComposedWith(
+        feedLoader: @escaping () -> FeedLoader.Publisher,
+        imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher
+    ) -> FeedViewController {
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
+        
+        let feedController = makeFeedViewController(
+            delegate: presentationAdapter,
+            title: FeedPresenter.title)
+        
+        presentationAdapter.presenter = FeedPresenter(
+            feedView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: imageLoader),
+            loadingView: WeakRefVirtualProxy(feedController),
+            errorView: WeakRefVirtualProxy(feedController))
+        
+        return feedController
+    }
+    
+    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+        feedController.delegate = delegate
+        feedController.title = title
+        return feedController
+    }
 }
