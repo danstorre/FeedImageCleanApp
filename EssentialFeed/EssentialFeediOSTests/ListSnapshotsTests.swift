1fd2dfc790bf89485cb47c27548464a061181de6
Daniel Torres <danstorre@gmail.com>
2022-11-27 16:40:53 -0400

    Extract shared ListViewController snapshot tests

--- EssentialFeed/EssentialFeediOSTests/ListSnapshotsTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ListSnapshotsTests.swift
@@ -2,24 +2,24 @@ import XCTest
 import EssentialFeediOS
 @testable import EssentialFeed
 
-class FeedSnapshotTests: XCTestCase {
+final class ListSnapshotsTests: XCTestCase {
     
-    func test_feedWithContent() {
+    func test_emptyFeed() {
         let sut = makeSUT()
         
-        sut.display(feedWithContent())
+        sut.display(emptyFeed())
         
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
     }
     
-    func test_feedWithFailedImageLoading() {
+    func test_listWithErrorMessage() {
         let sut = makeSUT()
         
-        sut.display(feedWithFailedImageLoading())
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
         
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
     }
     
     // MARK: - Helpers
@@ -33,34 +33,8 @@ class FeedSnapshotTests: XCTestCase {
         return controller
     }
     
-    private func feedWithContent() -> [ImageStub] {
-        return [
-            ImageStub(
-                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-                location: "East Side Gallery\nMemorial in Berlin, Germany",
-                image: UIImage.make(withColor: .red)
-            ),
-            ImageStub(
-                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-                location: "Garth Pier",
-                image: UIImage.make(withColor: .green)
-            )
-        ]
-    }
-    
-    private func feedWithFailedImageLoading() -> [ImageStub] {
-        return [
-            ImageStub(
-                description: nil,
-                location: "Cannon Street, London",
-                image: nil
-            ),
-            ImageStub(
-                description: nil,
-                location: "Brighton Seafront",
-                image: nil
-            )
-        ]
+    private func emptyFeed() -> [FeedImageCellController] {
+        return []
     }
     
     private func makeSnapshotURLFromTestBundle(with name: String) -> URL? {
@@ -135,44 +109,5 @@ class FeedSnapshotTests: XCTestCase {
         
         return data
     }
-}
-
-private extension ListViewController {
-    func display(_ stubs: [ImageStub]) {
-        let cells: [FeedImageCellController] = stubs.map { stub in
-            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub)
-            stub.controller = cellController
-            return cellController
-        }
-        
-        display(cells)
-    }
-}
-
-private class ImageStub: FeedImageCellControllerDelegate {
-    let viewModel: FeedImageViewModel
-    let image: UIImage?
-    weak var controller: FeedImageCellController?
     
-    init(description: String?, location: String?, image: UIImage?) {
-        self.viewModel = FeedImageViewModel(
-            description: description,
-            location: location)
-        
-        self.image = image
-    }
-    
-    func didRequestImage() {
-        controller?.display(ResourceLoadingViewModel(isLoading: false))
-        
-        if let image = image {
-            controller?.display(image)
-            controller?.display(ResourceErrorViewModel(message: .none))
-        } else {
-            controller?.display(ResourceErrorViewModel(message: "any"))
-        }
-    }
-    
-    func didCancelImageRequest() {}
 }
-
