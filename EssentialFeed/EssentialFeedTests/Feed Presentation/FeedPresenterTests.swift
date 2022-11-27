edfeb4c126bc9c2b585f7aa000312cad100ee93e
Daniel Torres <danstorre@gmail.com>
2022-11-27 00:20:06 -0400

    Remove unused FeedPresenter logic

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -1,103 +1,29 @@
-//
-//  Copyright © 2019 Essential Developer. All rights reserved.
-//
-
 import XCTest
 import EssentialFeed
 
 class FeedPresenterTests: XCTestCase {
 
-	func test_title_isLocalized() {
-		XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
-	}
+    func test_title_isLocalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
     
-    func test_map_createsViewModels() {
+    func test_map_createsViewModel() {
         let feed = uniqueImageFeed().models
         
         let viewModel = FeedPresenter.map(feed)
         
         XCTAssertEqual(viewModel.feed, feed)
     }
-
-	func test_init_doesNotSendMessagesToView() {
-		let (_, view) = makeSUT()
-
-		XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
-	}
-	
-	func test_didStartLoadingFeed_displaysNoErrorMessageAndStartsLoading() {
-		let (sut, view) = makeSUT()
-
-		sut.didStartLoadingFeed()
-
-		XCTAssertEqual(view.messages, [
-			.display(errorMessage: .none),
-			.display(isLoading: true)
-		])
-	}
-	
-	func test_didFinishLoadingFeed_displaysFeedAndStopsLoading() {
-		let (sut, view) = makeSUT()
-		let feed = uniqueImageFeed().models
-		
-		sut.didFinishLoadingFeed(with: feed)
-		
-		XCTAssertEqual(view.messages, [
-			.display(feed: feed),
-			.display(isLoading: false)
-		])
-	}
-	
-	func test_didFinishLoadingFeedWithError_displaysLocalizedErrorMessageAndStopsLoading() {
-		let (sut, view) = makeSUT()
-		
-		sut.didFinishLoadingFeed(with: anyNSError())
-		
-		XCTAssertEqual(view.messages, [
-            .display(errorMessage: localized("GENERIC_CONNECTION_ERROR", table: "Shared")),
-			.display(isLoading: false)
-		])
-	}
-	
-	// MARK: - Helpers
-
-	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
-		let view = ViewSpy()
-		let sut = FeedPresenter(feedView: view, loadingView: view, errorView: view)
-		trackForMemoryLeaks(view, file: file, line: line)
-		trackForMemoryLeaks(sut, file: file, line: line)
-		return (sut, view)
-	}
+    
+    // MARK: - Helpers
 
     private func localized(_ key: String, table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
-		let bundle = Bundle(for: FeedPresenter.self)
-		let value = bundle.localizedString(forKey: key, value: nil, table: table)
-		if value == key {
-			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
-		}
-		return value
-	}
-
-	private class ViewSpy: FeedView, ResourceLoadingView, ResourceErrorView {
-		enum Message: Hashable {
-			case display(errorMessage: String?)
-			case display(isLoading: Bool)
-			case display(feed: [FeedImage])
-		}
-		
-		private(set) var messages = Set<Message>()
-		
-		func display(_ viewModel: ResourceErrorViewModel) {
-			messages.insert(.display(errorMessage: viewModel.message))
-		}
-		
-		func display(_ viewModel: ResourceLoadingViewModel) {
-			messages.insert(.display(isLoading: viewModel.isLoading))
-		}
-		
-		func display(_ viewModel: FeedViewModel) {
-			messages.insert(.display(feed: viewModel.feed))
-		}
-	}
+        let bundle = Bundle(for: FeedPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
 
 }
