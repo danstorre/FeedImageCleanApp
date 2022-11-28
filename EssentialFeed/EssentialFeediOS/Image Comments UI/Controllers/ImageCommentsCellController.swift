7dbb4c354f84723c8d89d9c94bbbfc89847b5cba
Daniel Torres <danstorre@gmail.com>
2022-11-28 18:48:05 -0400

    Add default implementation for optional methods in the protocol

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentsCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentsCellController.swift
@@ -15,12 +15,4 @@ public class ImageCommentCellController: CellController {
         cell.dateLabel.text = model.date
         return cell
     }
-
-    public func preload() {
-
-    }
-
-    public func cancelLoad() {
-
-    }
 }
