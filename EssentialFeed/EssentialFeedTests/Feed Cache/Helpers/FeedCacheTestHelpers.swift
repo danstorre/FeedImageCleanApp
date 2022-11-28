4aced9548c5dc46bbcdba134aac61f11757f7123
Daniel Torres <danstorre@gmail.com>
2022-11-28 10:13:19 -0400

    Image Comments Presentation Feature

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -16,21 +16,11 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 }
 
 extension Date {
-	func minusFeedCacheMaxAge() -> Date {
-		return adding(days: -feedCacheMaxAgeInDays)
-	}
-	
-	private var feedCacheMaxAgeInDays: Int {
-		return 7
-	}
-	
-	private func adding(days: Int) -> Date {
-		return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-	}
-}
-
-extension Date {
-	func adding(seconds: TimeInterval) -> Date {
-		return self + seconds
-	}
+    func minusFeedCacheMaxAge() -> Date {
+        return adding(days: -feedCacheMaxAgeInDays)
+    }
+    
+    private var feedCacheMaxAgeInDays: Int {
+        return 7
+    }
 }
---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+
+}
