af23b5da922bee4f6bc7be64a3b4423d5b9c66ec
Daniel Torres <danstorre@gmail.com>
2022-12-11 11:33:14 -0400

    Does not load more after loading last page

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -3,23 +3,38 @@ import Combine
 import EssentialFeed
 
 public extension Paginated {
-     var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
-         guard let loadMore = loadMore else { return nil }
-
-         return {
-             Deferred {
-                 Future(loadMore)
-             }.eraseToAnyPublisher()
-         }
-     }
- }
+    init(items: [Item], loadMorePublisher: (() -> AnyPublisher<Self, Error>)?) {
+        self.init(items: items, loadMore: loadMorePublisher.map { publisher in
+            return { completion in
+                publisher().subscribe(Subscribers.Sink(receiveCompletion: { result in
+                    if case let .failure(error) = result {
+                        completion(.failure(error))
+                        
+                    }
+                }, receiveValue: { result in
+                    completion(.success(result))
+                }))
+            }
+        })
+    }
+    
+    var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
+        guard let loadMore = loadMore else { return nil }
+        
+        return {
+            Deferred {
+                Future(loadMore)
+            }.eraseToAnyPublisher()
+        }
+    }
+}
 
 public extension HTTPClient {
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
-
+    
     func getPublisher(url: URL) -> Publisher {
         var task: HTTPClientTask?
-
+        
         return Deferred {
             Future { completion in
                 task = self.get(from: url, completion: completion)
