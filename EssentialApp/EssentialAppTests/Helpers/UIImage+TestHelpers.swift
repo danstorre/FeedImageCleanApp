9a942adfec198e30d4d755166d389bc94bffe2ed
Daniel Torres <danstorre@gmail.com>
2022-12-11 01:00:11 -0400

    uses current trait collection so that images don't fail.

--- EssentialApp/EssentialAppTests/Helpers/UIImage+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/UIImage+TestHelpers.swift
@@ -6,13 +6,14 @@ import UIKit
 
 extension UIImage {
 	static func make(withColor color: UIColor) -> UIImage {
-		let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-		UIGraphicsBeginImageContext(rect.size)
-		let context = UIGraphicsGetCurrentContext()!
-		context.setFillColor(color.cgColor)
-		context.fill(rect)
-		let img = UIGraphicsGetImageFromCurrentImageContext()
-		UIGraphicsEndImageContext()
-		return img!
+        let imageSize = CGSize(width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat(for: .current)
+        format.scale = 1
+        let renderer = UIGraphicsImageRenderer.init(size: imageSize, format: format)
+        
+        return renderer.image { context in
+            color.setFill()
+            context.fill(.init(origin: .zero, size: imageSize))
+        }
 	}
 }
