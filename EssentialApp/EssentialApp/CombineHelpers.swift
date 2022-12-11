abeeb500c3eda532da09f561f6d130cdc09fea15
Daniel Torres <danstorre@gmail.com>
2022-12-11 11:14:10 -0400

    Prevent load more action while loading more

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -2,6 +2,18 @@ import Foundation
 import Combine
 import EssentialFeed
 
+public extension Paginated {
+     var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
+         guard let loadMore = loadMore else { return nil }
+
+         return {
+             Deferred {
+                 Future(loadMore)
+             }.eraseToAnyPublisher()
+         }
+     }
+ }
+
 public extension HTTPClient {
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
 
--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -75,6 +75,9 @@ class FeedUIIntegrationTests: XCTestCase {
         
         sut.simulateLoadMoreFeedAction()
         XCTAssertEqual(loader.loadMoreCallCount, 1, "Expected load more request")
+        
+        sut.simulateLoadMoreFeedAction()
+        XCTAssertEqual(loader.loadMoreCallCount, 1, "Expected no request while loading more")
     }
 	
 	func test_loadFeedCompletion_doesNotAlterCurrentRenderingStateOnError() {
