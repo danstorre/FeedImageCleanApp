b7c3947afdba39fc1b226e7602c341d3d49d93f0
Daniel Torres <danstorre@gmail.com>
2022-12-12 19:42:38 -0400

    Make in memory FeedImageDataStore implementation sync

---
+++ EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -0,0 +1,55 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+class InMemoryFeedStore {
+    private(set) var feedCache: CachedFeed?
+    private var feedImageDataCache: [URL: Data] = [:]
+    
+    private init(feedCache: CachedFeed? = nil) {
+        self.feedCache = feedCache
+    }
+}
+
+extension InMemoryFeedStore: FeedStore {
+    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+        feedCache = nil
+        completion(.success(()))
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        feedCache = CachedFeed(feed: feed, timestamp: timestamp)
+        completion(.success(()))
+    }
+    
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.success(feedCache))
+    }
+}
+
+extension InMemoryFeedStore: FeedImageDataStore {
+    func insert(_ data: Data, for url: URL) throws {
+        feedImageDataCache[url] = data
+    }
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        feedImageDataCache[url]
+    }
+}
+
+extension InMemoryFeedStore {
+    static var empty: InMemoryFeedStore {
+        InMemoryFeedStore()
+    }
+    
+    static var withExpiredFeedCache: InMemoryFeedStore {
+        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
+    }
+    
+    static var withNonExpiredFeedCache: InMemoryFeedStore {
+        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
+    }
+}
--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -5,34 +5,34 @@
 import CoreData
 
 public final class CoreDataFeedStore {
-	private static let modelName = "FeedStore"
-	private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
-	
-	private let container: NSPersistentContainer
-	private let context: NSManagedObjectContext
-
-	enum StoreError: Error {
-		case modelNotFound
-		case failedToLoadPersistentContainer(Error)
-	}
-
-	public init(storeURL: URL) throws {
-		guard let model = CoreDataFeedStore.model else {
-			throw StoreError.modelNotFound
-		}
-		
-		do {
-			container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
-			context = container.newBackgroundContext()
-		} catch {
-			throw StoreError.failedToLoadPersistentContainer(error)
-		}
-	}
-
-	func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
-		let context = self.context
-		context.perform { action(context) }
-	}
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataFeedStore.model else {
+            throw StoreError.modelNotFound
+        }
+        
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
+    }
+    
+    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
     
     func performSync<R>(_ action: (NSManagedObjectContext) -> Result<R, Error>) throws -> R {
         let context = self.context
@@ -40,15 +40,15 @@ public final class CoreDataFeedStore {
         context.performAndWait { result = action(context) }
         return try result.get()
     }
-	
-	private func cleanUpReferencesToPersistentStores() {
-		context.performAndWait {
-			let coordinator = self.container.persistentStoreCoordinator
-			try? coordinator.persistentStores.forEach(coordinator.remove)
-		}
-	}
-	
-	deinit {
-		cleanUpReferencesToPersistentStores()
-	}
+    
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+    
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
 }
