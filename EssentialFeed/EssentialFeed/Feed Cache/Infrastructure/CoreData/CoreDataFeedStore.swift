71dd4fb2c220a9c3f428c51db254e003b87a4f09
Daniel Torres <danstorre@gmail.com>
2022-12-12 19:39:32 -0400

    Make CoreData FeedImageDataStore implementation sync

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -33,6 +33,13 @@ public final class CoreDataFeedStore {
 		let context = self.context
 		context.perform { action(context) }
 	}
+    
+    func performSync<R>(_ action: (NSManagedObjectContext) -> Result<R, Error>) throws -> R {
+        let context = self.context
+        var result: Result<R, Error>!
+        context.performAndWait { result = action(context) }
+        return try result.get()
+    }
 	
 	private func cleanUpReferencesToPersistentStores() {
 		context.performAndWait {
