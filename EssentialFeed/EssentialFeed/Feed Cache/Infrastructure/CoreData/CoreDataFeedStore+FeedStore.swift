1a073e870a8353f7783c02287887c0b65cf99df8
Daniel Torres <danstorre@gmail.com>
2022-12-12 20:40:23 -0400

    Remove unused async methods

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -5,34 +5,34 @@
 import CoreData
 
 extension CoreDataFeedStore: FeedStore {
-	
-	public func retrieve(completion: @escaping RetrievalCompletion) {
-		performAsync { context in
-			completion(Result {
-				try ManagedCache.find(in: context).map {
-					CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-				}
-			})
-		}
-	}
-	
-	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		performAsync { context in
-			completion(Result {
-				let managedCache = try ManagedCache.newUniqueInstance(in: context)
-				managedCache.timestamp = timestamp
-				managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-				try context.save()
-			})
-		}
-	}
-	
-	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		performAsync { context in
-			completion(Result {
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        performAsync { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map {
+                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
+                }
+            })
+        }
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        performAsync { context in
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+                try context.save()
+            })
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        performAsync { context in
+            completion(Result {
                 try ManagedCache.deleteCache(in: context)
-			})
-		}
-	}
-	
+            })
+        }
+    }
+    
 }
