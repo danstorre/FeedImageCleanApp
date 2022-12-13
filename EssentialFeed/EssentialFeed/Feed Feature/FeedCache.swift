2529202ff8ec45ba17187fa87fdb98eb4dd9fd2f
Daniel Torres <danstorre@gmail.com>
2022-12-12 21:24:43 -0400

    Make FeedCache sync

--- EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 public protocol FeedCache {
-    typealias SaveResult = Result<Void, Error>
-
-    func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void)
+    func save(_ feed: [FeedImage]) throws
 }
